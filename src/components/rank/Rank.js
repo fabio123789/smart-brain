d0c169264c82a0f26c36e1ea3a508eaa449baf0c
fabio123789 <goncalvesfabio227@gmail.com>
2024-05-17 18:58:23 +0100

    update

--- src/components/rank/Rank.js
+++ src/components/rank/Rank.js
@@ -3,8 +3,8 @@ import React from "react";
 const Rank = ({ name, entries }) => {
   return (
     <div>
-      <div className="white f3">{`${name}, your rank is....`}</div>
-      <div className="white f1">{`#${entries}`}</div>
+      <div className="white f3">{`${name}, your number of entries is....`}</div>
+      <div className="white f1">{`${entries}`}</div>
     </div>
   );
 };
